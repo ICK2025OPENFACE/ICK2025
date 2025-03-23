7fafcfa946f6a8f44db261bc7bc4fd9c2e497b61
theSPGN <spgn1226@gmail.com>
2025-03-23 13:12:15 +0100

    Adding basic gesture code recognition

---
+++ gesture.py
@@ -0,0 +1,63 @@
+import time
+import mediapipe as mp
+import cv2
+
+gestures = []
+
+model_path = "gesture_recognizer.task"
+BaseOptions = mp.tasks.BaseOptions
+GestureRecognizer = mp.tasks.vision.GestureRecognizer
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+
+def print_result(
+    result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int
+):
+    global gestures
+    try:
+        gesture = result.gestures[0][0].category_name
+        if gesture and gesture != "None":
+            gestures.append(gesture)
+            print("Recognized Gesture:", gestures[-1])
+    except:
+        pass
+
+    if len(gestures) > 10:
+        gestures.pop(0)
+
+
+cam = cv2.VideoCapture(0)
+options = GestureRecognizerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result,
+)
+
+with GestureRecognizer.create_from_options(options) as recognizer:
+    last_recognition_time = 0
+    recognition_interval = 0.1
+
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("Failed to capture frame. Exiting...")
+            break
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
+
+        current_time = time.time()
+        if current_time - last_recognition_time >= recognition_interval:
+            recognizer.recognize_async(
+                mp_image, int(cv2.getTickCount() / cv2.getTickFrequency() * 1000)
+            )
+            last_recognition_time = current_time
+
+        cv2.imshow("Camera", frame)
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    cam.release()
+    cv2.destroyAllWindows()
