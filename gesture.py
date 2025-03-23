f55db865455c444608f81a0344cf3655824888e0
theSPGN <spgn1226@gmail.com>
2025-03-23 13:33:00 +0100

    Added callback for last gesture

--- gesture.py
+++ gesture.py
@@ -2,7 +2,13 @@ import time
 import mediapipe as mp
 import cv2
 
+# Initializing the array for the gestures
 gestures = []
+last_gesture = None
+MAX_GESTURES_ARRAY_LENGTH = 10
+
+
+# Initializing the recognition model
 # https://ai.google.dev/edge/mediapipe/solutions/vision/gesture_recognizer/index#models
 model_path = "gesture_recognizer.task"
 BaseOptions = mp.tasks.BaseOptions
@@ -12,52 +18,72 @@ GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 
-def print_result(
+# printing last gesture
+def view_last_gesture():
+    global last_gesture
+    print(last_gesture)
+
+
+# saving the recognition restult to array for sequential gestures
+def save_result(
     result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int
 ):
     global gestures
+    global last_gesture
     try:
         gesture = result.gestures[0][0].category_name
+        last_gesture = gesture
         if gesture and gesture != "None":
             gestures.append(gesture)
-            print("Recognized Gesture:", gestures[-1])
     except:
-        pass
+        last_gesture = None
 
-    if len(gestures) > 10:
+    if len(gestures) > MAX_GESTURES_ARRAY_LENGTH:
         gestures.pop(0)
 
 
-cam = cv2.VideoCapture(0)
-options = GestureRecognizerOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result,
-)
+if __name__ == "__main__":
 
-with GestureRecognizer.create_from_options(options) as recognizer:
-    last_recognition_time = 0
-    recognition_interval = 0.1
+    cam = cv2.VideoCapture(0)
+    options = GestureRecognizerOptions(
+        base_options=BaseOptions(model_asset_path=model_path),
+        running_mode=VisionRunningMode.LIVE_STREAM,
+        result_callback=save_result,
+    )
 
-    while True:
-        ret, frame = cam.read()
-        if not ret:
-            print("Failed to capture frame. Exiting...")
-            break
+    show_camera = True
 
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
+    with GestureRecognizer.create_from_options(options) as recognizer:
+        last_recognition_time = 0
+        recognition_interval = 0.1
 
-        current_time = time.time()
-        if current_time - last_recognition_time >= recognition_interval:
-            recognizer.recognize_async(
-                mp_image, int(cv2.getTickCount() / cv2.getTickFrequency() * 1000)
-            )
-            last_recognition_time = current_time
+        while True:
+            try:
+                ret, frame = cam.read()
+                if not ret:
+                    print("Failed to capture frame. Exiting...")
+                    break
 
-        cv2.imshow("Camera", frame)
-        if cv2.waitKey(1) == ord("q"):
-            break
+                frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
 
-    cam.release()
-    cv2.destroyAllWindows()
+                current_time = time.time()
+                if current_time - last_recognition_time >= recognition_interval:
+                    recognizer.recognize_async(
+                        mp_image,
+                        int(cv2.getTickCount() / cv2.getTickFrequency() * 1000),
+                    )
+                    last_recognition_time = current_time
+
+                    view_last_gesture()
+
+                if show_camera:
+                    cv2.imshow("Camera", frame)
+                    if cv2.waitKey(1) == ord("q"):
+                        break
+
+            except Exception as e:
+                print(f"Unhandled exception: {e}")
+        cam.release()
+        if show_camera:
+            cv2.destroyAllWindows()
